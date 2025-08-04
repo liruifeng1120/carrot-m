43c968c1268fb5c36c923781dc0243701c841039
liruifeng1120 <2218808853@qq.com>
2025-08-05 00:50:32 +0800

    解决编译失败问题

--- selfdrive/ui/qt/offroad/settings.h
+++ selfdrive/ui/qt/offroad/settings.h
@@ -134,7 +134,7 @@ class CValueControl : public AbstractControl {
   Q_OBJECT
 
 public:
-  CValueControl(const QString& params, const QString& title, const QString& desc, int min, int max, int unit = 1);
+  CValueControl(const QString& params, const QString& title, const QString& desc, const QString& icon, int min, int max, int unit = 1);
 
 private slots:
   void increaseValue();
