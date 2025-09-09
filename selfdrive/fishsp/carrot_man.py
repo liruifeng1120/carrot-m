7eabc47c8ada7e0b50b69451650737e2ac96d005
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-09-09 17:57:08 +0800

    初步增加carrot_man

---
+++ selfdrive/fishsp/carrot_man.py
@@ -0,0 +1,440 @@
+import fcntl
+import json
+import math
+import os
+import socket
+import struct
+import subprocess
+import threading
+import time
+import numpy as np
+import zmq
+from datetime import datetime
+
+from ftplib import FTP
+from cereal import log
+import cereal.messaging as messaging
+from openpilot.common.realtime import Ratekeeper
+from openpilot.common.params import Params
+from openpilot.common.filter_simple import MyMovingAverage
+from openpilot.system.hardware import PC, TICI
+from openpilot.selfdrive.navd.helpers import Coordinate
+from opendbc.car.common.conversions import Conversions as CV
+
+try:
+  from shapely.geometry import LineString
+  SHAPELY_AVAILABLE = True
+except ImportError:
+  SHAPELY_AVAILABLE = False
+
+NetworkType = log.DeviceState.NetworkType
+
+nav_type_mapping = {
+  12: ("turn", "left", 1),
+  16: ("turn", "sharp left", 1),
+  1000: ("turn", "slight left", 1),
+  1001: ("turn", "slight right", 2),
+  1002: ("fork", "slight left", 3),
+  1003: ("fork", "slight right", 4),
+  1006: ("off ramp", "left", 3),
+  1007: ("off ramp", "right", 4),
+  13: ("turn", "right", 2),
+  19: ("turn", "sharp right", 2),
+  102: ("off ramp", "slight left", 3),
+  105: ("off ramp", "slight left", 3),
+  112: ("off ramp", "slight left", 3),
+  115: ("off ramp", "slight left", 3),
+  101: ("off ramp", "slight right", 4),
+  104: ("off ramp", "slight right", 4),
+  111: ("off ramp", "slight right", 4),
+  114: ("off ramp", "slight right", 4),
+  7: ("fork", "left", 3),
+  44: ("fork", "left", 3),
+  17: ("fork", "left", 3),
+  75: ("fork", "left", 3),
+  76: ("fork", "left", 3),
+  118: ("fork", "left", 3),
+  6: ("fork", "right", 4),
+  43: ("fork", "right", 4),
+  73: ("fork", "right", 4),
+  74: ("fork", "right", 4),
+  123: ("fork", "right", 4),
+  124: ("fork", "right", 4),
+  117: ("fork", "right", 4),
+  131: ("rotary", "slight right", 5),
+  132: ("rotary", "slight right", 5),
+  140: ("rotary", "slight left", 5),
+  141: ("rotary", "slight left", 5),
+  133: ("rotary", "right", 5),
+  134: ("rotary", "sharp right", 5),
+  135: ("rotary", "sharp right", 5),
+  136: ("rotary", "sharp left", 5),
+  137: ("rotary", "sharp left", 5),
+  138: ("rotary", "sharp left", 5),
+  139: ("rotary", "left", 5),
+  142: ("rotary", "straight", 5),
+  14: ("turn", "uturn", 5),
+  201: ("arrive", "straight", 5),
+  51: ("notification", "straight", None),
+  52: ("notification", "straight", None),
+  53: ("notification", "straight", None),
+  54: ("notification", "straight", None),
+  55: ("notification", "straight", None),
+  153: ("", "", 6),  #TG
+  154: ("", "", 6),  #TG
+  249: ("", "", 6)   #TG
+}
+
+################ CarrotNavi
+## 국가법령정보센터: 도로설계기준
+#V_CURVE_LOOKUP_BP = [0., 1./800., 1./670., 1./560., 1./440., 1./360., 1./265., 1./190., 1./135., 1./85., 1./55., 1./30., 1./15.]
+#V_CRUVE_LOOKUP_VALS = [300, 150, 120, 110, 100, 90, 80, 70, 60, 50, 45, 35, 30]
+V_CURVE_LOOKUP_BP = [0., 1./800., 1./670., 1./560., 1./440., 1./360., 1./265., 1./190., 1./135., 1./85., 1./55., 1./30., 1./25.]
+V_CRUVE_LOOKUP_VALS = [300, 150, 120, 110, 100, 90, 80, 70, 60, 50, 40, 15, 5]
+
+# Haversine formula to calculate distance between two GPS coordinates
+#haversine_cache = {}
+def haversine(lon1, lat1, lon2, lat2):
+    #key = (lon1, lat1, lon2, lat2)
+    #if key in haversine_cache:
+    #    return haversine_cache[key]
+
+    R = 6371000  # Radius of Earth in meters
+    phi1, phi2 = math.radians(lat1), math.radians(lat2)
+    dphi = math.radians(lat2 - lat1)
+    dlambda = math.radians(lon2 - lon1)
+
+    a = math.sin(dphi / 2) ** 2 + math.cos(phi1) * math.cos(phi2) * math.sin(dlambda / 2) ** 2
+    distance = 2 * R * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    #haversine_cache[key] = distance
+    return distance
+
+
+# Get the closest point on a segment between two coordinates
+def closest_point_on_segment(p1, p2, current_position):
+    x1, y1 = p1
+    x2, y2 = p2
+    px, py = current_position
+
+    dx = x2 - x1
+    dy = y2 - y1
+    if dx == 0 and dy == 0:
+        return p1  # p1 and p2 are the same point
+
+    # Parameter t is the projection factor onto the line segment
+    t = ((px - x1) * dx + (py - y1) * dy) / (dx * dx + dy * dy)
+    t = max(0, min(1, t))  # Clamp t to the segment
+
+    closest_x = x1 + t * dx
+    closest_y = y1 + t * dy
+
+    return (closest_x, closest_y)
+
+
+# Get path after a certain distance from the current position
+def get_path_after_distance(start_index, coordinates, current_position, distance_m):
+    total_distance = 0
+    path_after_distance = []
+    closest_index = -1
+    closest_point = None
+    min_distance = float('inf')
+
+    start_index = max(0, start_index - 2)
+
+    # 가까운 점만 탐색하도록 수정
+    for i in range(start_index, len(coordinates) - 1):
+        p1 = coordinates[i]
+        p2 = coordinates[i + 1]
+        candidate_point = closest_point_on_segment(p1, p2, current_position)
+        distance = haversine(current_position[0], current_position[1], candidate_point[0], candidate_point[1])
+
+        if distance < min_distance:
+            min_distance = distance
+            closest_point = candidate_point
+            closest_index = i
+        elif distance > min_distance and min_distance < 10:
+            break
+
+    start_index = closest_index
+    # Start from the closest point and calculate the path after the specified distance
+    if closest_index != -1:
+        path_after_distance.append(closest_point)
+
+        path_after_distance.append(coordinates[closest_index + 1])
+        total_distance = haversine(closest_point[0], closest_point[1], coordinates[closest_index + 1][0],
+                                   coordinates[closest_index + 1][1])
+
+        # Traverse the path forward from the next point
+        for i in range(closest_index + 1, len(coordinates) - 1):
+            coord1 = coordinates[i]
+            coord2 = coordinates[i + 1]
+            segment_distance = haversine(coord1[0], coord1[1], coord2[0], coord2[1])
+
+            if total_distance + segment_distance >= distance_m and segment_distance > 0:
+                remaining_distance = distance_m - total_distance
+                ratio = remaining_distance / segment_distance
+                interpolated_lon = coord1[0] + ratio * (coord2[0] - coord1[0])
+                interpolated_lat = coord1[1] + ratio * (coord2[1] - coord1[1])
+                path_after_distance.append((interpolated_lon, interpolated_lat))
+                break
+
+            total_distance += segment_distance
+            path_after_distance.append(coord2)
+
+    return path_after_distance, start_index, closest_point
+
+
+def calculate_angle(point1, point2):
+    delta_lon = point2[0] - point1[0]
+    delta_lat = point2[1] - point1[1]
+    return math.degrees(math.atan2(delta_lat, delta_lon))
+
+# Convert GPS coordinates to relative x, y coordinates based on a reference point and heading
+def gps_to_relative_xy(gps_path, reference_point, heading_deg):
+    ref_lon, ref_lat = reference_point
+    relative_coordinates = []
+
+    # Convert heading from degrees to radians
+    heading_rad = math.radians(heading_deg)
+
+    for lon, lat in gps_path:
+        # Convert lat/lon differences to meters (assuming small distances for simple approximation)
+        x = (lon - ref_lon) * 40008000 * math.cos(math.radians(ref_lat)) / 360
+        y = (lat - ref_lat) * 40008000 / 360
+
+        # Rotate coordinates based on the heading angle to align with the car's direction
+        x_rot = x * math.cos(heading_rad) - y * math.sin(heading_rad)
+        y_rot = x * math.sin(heading_rad) + y * math.cos(heading_rad)
+
+        relative_coordinates.append((y_rot, x_rot))
+
+    return relative_coordinates
+
+
+# Calculate curvature given three points using a faster vector-based method
+#curvature_cache = {}
+def calculate_curvature(p1, p2, p3):
+    #key = (p1, p2, p3)
+    #if key in curvature_cache:
+    #    return curvature_cache[key]
+
+    v1 = (p2[0] - p1[0], p2[1] - p1[1])
+    v2 = (p3[0] - p2[0], p3[1] - p2[1])
+
+    cross_product = v1[0] * v2[1] - v1[1] * v2[0]
+    len_v1 = math.sqrt(v1[0] ** 2 + v1[1] ** 2)
+    len_v2 = math.sqrt(v2[0] ** 2 + v2[1] ** 2)
+
+    if len_v1 * len_v2 == 0:
+        curvature = 0
+    else:
+        curvature = cross_product / (len_v1 * len_v2 * len_v1)
+
+    #curvature_cache[key] = curvature
+    return curvature
+
+class CarrotMan:
+  def __init__(self):
+    print("************************************************CarrotMan init************************************************")
+    self.params = Params()
+    self.params_memory = Params("/dev/shm/params")
+    #self.sm = messaging.SubMaster(['deviceState', 'carState', 'controlsState', 'longitudinalPlan', 'modelV2', 'selfdriveState', 'carControl', 'navRouteNavd', 'liveLocationKalman', 'navInstruction'])
+    self.sm = messaging.SubMaster(['deviceState', 'carState', 'controlsState', 'longitudinalPlan', 'modelV2', 'carControl', 'liveLocationKalman', 'navInstruction'])
+    self.pm = messaging.PubMaster(['carrotMan', "navRoute", "navInstructionCarrot"])
+
+    #self.carrot_serv = CarrotServ()
+
+    self.show_panda_debug = False
+    self.broadcast_ip = self.get_broadcast_address()
+    self.broadcast_port = 7705
+    self.carrot_man_port = 7706
+    self.connection = None
+
+    self.ip_address = "0.0.0.0"
+    self.remote_addr = None
+
+    self.turn_speed_last = 250
+    self.curvatureFilter = MyMovingAverage(20)
+    ##self.carrot_curve_speed_params()
+
+    ##self.carrot_zmq_thread = threading.Thread(target=self.carrot_cmd_zmq, args=[])
+    ##self.carrot_zmq_thread.daemon = True
+    ##self.carrot_zmq_thread.start()
+
+    ##self.carrot_panda_debug_thread = threading.Thread(target=self.carrot_panda_debug, args=[])
+    ##self.carrot_panda_debug_thread.daemon = True
+    ##self.carrot_panda_debug_thread.start()
+
+    ##self.carrot_route_thread = threading.Thread(target=self.carrot_route, args=[])
+    ##self.carrot_route_thread.daemon = True
+    ##self.carrot_route_thread.start()
+
+    self.is_running = True
+    threading.Thread(target=self.broadcast_version_info).start()
+
+    self.navi_points = []
+    self.navi_points_start_index = 0
+    self.navi_points_active = False
+    self.navd_active = False
+
+    self.active_carrot_last = False
+
+    self.is_metric = self.params.get_bool("IsMetric")
+
+  def get_broadcast_address(self):
+    if PC:
+      iface = b'br0'
+    else:
+      iface = b'wlan0'
+    try:
+      with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        ip = fcntl.ioctl(
+          s.fileno(),
+          0x8919,
+          struct.pack('256s', iface)
+        )[20:24]
+        return socket.inet_ntoa(ip)
+    except (OSError, Exception):
+      return None
+
+  def get_local_ip(self):
+      try:
+          # 외부 서버와의 연결을 통해 로컬 IP 확인
+          with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+              s.connect(("8.8.8.8", 80))  # Google DNS로 연결 시도
+              return s.getsockname()[0]
+      except Exception as e:
+          return f"Error: {e}"
+
+  # 브로드캐스트 메시지 전송
+  def broadcast_version_info(self):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    frame = 0
+    ##self.save_toggle_values()
+    rk = Ratekeeper(10, print_delay_threshold=None)
+
+    ##carrotIndex_last = self.carrot_serv.carrotIndex
+    while self.is_running:
+      try:
+        self.sm.update(0)
+        ##if self.sm.updated['navRouteNavd']:
+        ##  self.send_routes(self.sm['navRouteNavd'].coordinates, True)
+        remote_addr = self.remote_addr
+        remote_ip = remote_addr[0] if remote_addr is not None else ""
+        ##vturn_speed = self.carrot_curve_speed(self.sm)
+        ##coords, distances, route_speed = self.carrot_navi_route()
+
+        #print("coords=", coords)
+        #print("curvatures=", curvatures)
+        ##self.carrot_serv.update_navi(remote_ip, self.sm, self.pm, vturn_speed, coords, distances, route_speed)
+
+        if frame % 20 == 0 or remote_addr is not None:
+          try:
+            self.broadcast_ip = self.get_broadcast_address() if remote_addr is None else remote_addr[0]
+            if not PC:
+              ip_address = socket.gethostbyname(socket.gethostname())
+            else:
+              ip_address = self.get_local_ip()
+            if ip_address != self.ip_address:
+              self.ip_address = ip_address
+              self.remote_addr = None
+            self.params_memory.put_nonblocking("NetworkAddress", self.ip_address)
+
+            msg = self.make_send_message()
+            if self.broadcast_ip is not None:
+              dat = msg.encode('utf-8')
+              sock.sendto(dat, (self.broadcast_ip, self.broadcast_port))
+            #for i in range(1, 255):
+            #  ip_tuple = socket.inet_aton(self.broadcast_ip)
+            #  new_ip = ip_tuple[:-1] + bytes([i])
+            #  address = (socket.inet_ntoa(new_ip), self.broadcast_port)
+            #  sock.sendto(dat, address)
+
+            if remote_addr is None:
+              print(f"Broadcasting: {self.broadcast_ip}:{msg}")
+              if not self.navd_active:
+                #print("clear path_points: navd_active: ", self.navd_active)
+                self.navi_points = []
+                self.navi_points_active = False
+
+          except Exception as e:
+            if self.connection:
+              self.connection.close()
+            self.connection = None
+            print(f"##### broadcast_error...: {e}")
+            traceback.print_exc()
+
+        rk.keep_time()
+        frame += 1
+      except Exception as e:
+        print(f"broadcast_version_info error...: {e}")
+        traceback.print_exc()
+        time.sleep(1)
+
+  def make_send_message(self):
+    msg = {}
+    msg['Carrot2'] = self.params.get("Version").decode('utf-8')
+    isOnroad = self.params.get_bool("IsOnroad")
+    msg['IsOnroad'] = isOnroad
+    msg['CarrotRouteActive'] = self.navi_points_active
+    msg['ip'] = self.ip_address
+    msg['port'] = self.carrot_man_port
+    self.controls_active = False
+    self.xState = 0
+    self.trafficState = 0
+    v_ego_kph = 0
+    log_carrot = ""
+    v_cruise_kph = 0
+    if not isOnroad:
+      self.xState = 0
+      self.trafficState = 0
+    else:
+      if self.sm.alive['carState']:
+        carState = self.sm['carState']
+        v_ego_kph = int(carState.vEgoCluster * 3.6 + 0.5)
+        ##log_carrot = carState.logCarrot
+        v_cruise_kph = carState.vCruise
+      ##if self.sm.alive['selfdriveState']:
+      ##  selfdrive = self.sm['selfdriveState']
+      ##  self.controls_active = selfdrive.active
+      ##if self.sm.alive['longitudinalPlan']:
+      ##  lp = self.sm['longitudinalPlan']
+      ##  self.xState = lp.xState
+      ##  self.trafficState = lp.trafficState
+
+    ##msg['log_carrot'] = log_carrot
+    msg['v_cruise_kph'] = v_cruise_kph
+    msg['v_ego_kph'] = v_ego_kph
+    ##msg['tbt_dist'] = self.carrot_serv.xDistToTurn
+    ##msg['sdi_dist'] = self.carrot_serv.xSpdDist
+    msg['active'] = self.controls_active
+    msg['xState'] = self.xState
+    msg['trafficState'] = self.trafficState
+    return json.dumps(msg)
+
+
+
+
+import traceback
+
+def main():
+  print("CarrotManager Started")
+  #print("Carrot GitBranch = {}, {}".format(Params().get("GitBranch"), Params().get("GitCommitDate")))
+  carrot_man = CarrotMan()
+
+  print(f"CarrotMan {carrot_man}")
+  #threading.Thread(target=carrot_man.kisa_app_thread).start()
+  while True:
+    try:
+      #carrot_man.carrot_man_thread()
+      pass
+    except Exception as e:
+      print(f"carrot_man error...: {e}")
+      traceback.print_exc()
+      time.sleep(10)
+
+
+if __name__ == "__main__":
+  main()
